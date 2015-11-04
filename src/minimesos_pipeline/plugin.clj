29eb897969fd77c2bb514bc8ba14d33f8f31f791
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2015-11-04 14:06:14 +0100

    Error case when no agent for topic

--- src/minimesos_pipeline/plugin.clj
+++ src/minimesos_pipeline/plugin.clj
@@ -11,14 +11,16 @@
   (let [error-mode :continue
         error-handler (fn [failed-agent ^Exception exception]
                         (log/error (.getMessage exception)))
-        steps[:step-result-updated :step-finished]
+        steps [:step-result-updated :step-finished]
         agents (reset! agents (into {} (map #(vector % (agent {})) steps)))        
         ch (go-loop []
              (let [publ (:event-publisher ctx)
                    {:keys [topic payload]} (<! publ)
                    topic-agent (get agents topic)]
                (log/info "Received payload update: " topic payload)
-               (send-off topic-agent assoc :topic topic :payload payload))
+               (if (nil? topic-agent)
+                 (log/error "No agent for topic: " topic)
+                 (send-off topic-agent assoc :topic topic :payload payload)))
              (recur))]
     agents))
 
