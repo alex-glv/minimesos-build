4cd893815e0d16e0a1d468c4a5c7f6311ab74534
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2015-11-13 12:23:47 +0100

    Adding slack integration

--- src/minimesos_pipeline/api.clj
+++ src/minimesos_pipeline/api.clj
@@ -2,12 +2,14 @@
   (:use compojure.core)
   (require [clojure.string :as string]
            [ring.middleware.json :as ring-json]
+           [ring.middleware.keyword-params :as ring-kw]
            [minimesos-pipeline.pipeline :as pipeline]
            [cheshire.core :as ch]
            [cheshire.generate :as chg]
            [lambdacd.runners :as runners]
            [lambdacd.core :as lambdacd]
-           [lambdacd.event-bus :as event-bus]))
+           [lambdacd.event-bus :as event-bus]
+           [clojure.tools.logging :as log]))
 
 (defn json [data]
   {:headers { "Content-Type" "application/json"}
@@ -15,20 +17,33 @@
    :status 200 })
 
 (defn rest-api [ctx]
-  (ring-json/wrap-json-params
-   (context "/api" []
-    (POST "/github/pr/:pr" {{pr :pr} :params data :json-params}
-          (do
-            (event-bus/publish ctx :pr-trigger {:final-result {:status :success :step-name (str "Building PR " pr)}})
-            (pipeline/run-async (pipeline/get-pipeline :auto) ctx {:pr-id pr})
-            (json {:status :success})))
+  (-> (context "/api" []
+               (POST "/slack-github/tag" {{text "text" trigger "trigger"} :json-params :as data}
+                     (do
+                       (log/info "Slack build request with " text trigger (re-find #"build-tag " text))
+                       (let [tag-request (string/split text #" ")
+                             tag (second tag-request)]
+                         (if tag
+                           (do  (event-bus/publish ctx :tag-trigger {:final-result {:status :success :step-name (str "Building tag " tag)}})
+                                (pipeline/run-async (pipeline/get-pipeline :auto) ctx {:tag-id tag})
+                                (json {:status :success}))
+                           (json {:status :failed})))))
 
-    (POST "/github/tag/:tag" {{tag :tag} :params data :json-params}
-          (do
-            (event-bus/publish ctx :tag-trigger {:final-result {:status :success :step-name (str "Building tag " tag)}})
-            (pipeline/run-async (pipeline/get-pipeline :auto) ctx {:tag-id tag})
-            (json {:status :success})))
-    (POST "/github/commit" []
-          (do
-            (pipeline/run-async (pipeline/get-pipeline :auto) ctx {})
-            (json {:status :success}))))))
+               (POST "/slack-github/pr" {{text "text" trigger "trigger"} :json-params :as data}
+                     (do
+                       (log/info "Slack build request with " data)
+                       (let [pr-request (string/split text #" ")
+                             pr-no (second pr-request)]
+                         (if pr-no
+                           (do  (event-bus/publish ctx :pr-trigger {:final-result {:status :success :step-name (str "Building pr " pr-no)}})
+                                (pipeline/run-async (pipeline/get-pipeline :auto) ctx {:pr-id pr-no})
+                                (json {:status :success}))
+                           (json {:status :failed})))))
+               
+               (POST "/github/commit" []
+                     (do
+                       (pipeline/run-async (pipeline/get-pipeline :auto) ctx {})
+                       (json {:status :success}))))
+      ring-kw/wrap-keyword-params
+      ring-json/wrap-json-params
+      ))
