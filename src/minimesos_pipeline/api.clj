7b5ecffebc24e6bbd14abfba2b627003f72f05e1
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2015-11-13 14:41:52 +0100

    Fixing regexp

--- src/minimesos_pipeline/api.clj
+++ src/minimesos_pipeline/api.clj
@@ -24,7 +24,7 @@
              (do (log/info "Slack request: " (slurp body))
                  (let [body-text (slurp body)
                        body-parsed (into {} (map #(string/split % #"=") (string/split body-text  #"&")))
-                       [trigger-word trigger-type identifier] (string/split (get body-parsed "text") #"+")]
+                       [trigger-word trigger-type identifier] (string/split (get body-parsed "text") #"\\+")]
                    (log/info "Slack request: " body-parsed)
                    (case trigger-type
                      "pr" (do (event-bus/publish ctx :pr-trigger {:final-result {:status :success :step-name (str "Building pr " identifier)}})
